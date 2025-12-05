781645ecad0aec6815c105d1d22513b2c4879a14
Ян <89816834016@mail.ru>
2025-12-05 11:08:48 +0100

    fixed images

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,5 @@
 import { UnitSelector } from '../Selector/SelectorUnits';
+import logo from '../../assets/images/logo.svg'
 import './Header.css';
 
 export const Header = () => {
@@ -6,7 +7,7 @@ export const Header = () => {
     return (
         <header className="header">
             <div className="container flex">
-                <img className='logo' src="src\assets\images\logo.svg" alt="logo" />
+                <img className='logo' src={logo} alt="logo" />
                 <UnitSelector />
             </div>
         </header>
